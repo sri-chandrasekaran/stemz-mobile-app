39e243d57ecaa3ac9f1d58542abd09f1b614fe6b
MiguelALopez1 <miguel.a.lopez.083102@gmail.com>
2024-05-23 12:55:45 -0400

    added homepage with navigation working

---
+++ stemz-learning/app/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Login Page</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+});
---
+++ stemz-learning/app/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Text, View, StyleSheet, Image, TouchableOpacity, Pressable } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/images/stem_logo.png')} style={styles.image} />
+      <Text style={styles.title}>STEMz Learning</Text>
+      <Link href="/SignUp" asChild>
+        <Pressable style={styles.buttonSignUp}>
+          <Text style={styles.buttonSignUpText}>Sign Up</Text>
+        </Pressable>
+      </Link>
+      <Link href="/Login" asChild>
+        <Pressable style={styles.buttonLogin}>
+          <Text style={styles.buttonLoginText}>Login</Text>
+        </Pressable>
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    color: '#357717',
+    marginBottom: 60,
+  },
+  buttonSignUp: {
+    backgroundColor: '#357717',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginBottom: 10,
+    width: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonSignUpText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  buttonLogin: {
+    backgroundColor: '#357717',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    width: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonLoginText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
